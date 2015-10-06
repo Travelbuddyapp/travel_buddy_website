0ce375bfc8bc204c5f844290d8ad9f4213572be7
JordanSpencerWu <jordan.spencer.wu@gmail.com>
2015-10-06 16:42:29 -0600

    add devise to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :trips, dependent: :destroy
   has_many :vaccines, dependent: :destroy
   has_many :documents, dependent: :destroy
