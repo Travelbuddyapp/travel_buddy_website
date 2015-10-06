6e8286355ab2e419c83e7a74ddc54b9b31799fe0
JordanSpencerWu <jordan.spencer.wu@gmail.com>
2015-10-06 17:04:16 -0600

    add static page controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  root 'static_page#home'
 
   resources :addresses
   resources :contacts
@@ -12,7 +13,7 @@ Rails.application.routes.draw do
   resources :checklists do
     resources :list_items
   end
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
