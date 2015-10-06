e7140766a44c636c40ad993904fbe6a84f3ad4c5
JordanSpencerWu <jordan.spencer.wu@gmail.com>
2015-10-06 16:39:17 -0600

    add paperclip

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@ class User < ActiveRecord::Base
   has_many :contacts, dependent: :destroy
   has_one :address, dependent: :destroy
   validates :first_name, :last_name, :birth_date, :phone_number, :gender, presence: true
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
   
 end
