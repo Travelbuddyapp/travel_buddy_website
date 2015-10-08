e0d8df438d9a340758b2969bb79b9b85fc1fbeda
JordanSpencerWu <jordan.spencer.wu@gmail.com>
2015-10-07 18:57:32 -0600

    add rspec for reservation model

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Reservation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'attributes' do
+    it { should respond_to(:type) }
+    it { should respond_to(:business_name) }
+    it { should respond_to(:confirmation_number) }
+    it { should respond_to(:note) }
+    it { should respond_to(:check_in_date) }
+    it { should respond_to(:check_out_date) }
+    it { should validate_presence_of(:type) }
+    it { should belong_to(:trip) }
+    it { should have_one(:address) }
+  end
 end
