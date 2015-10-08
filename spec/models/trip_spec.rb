59e25451fe72f710deebee2af66fa3b2cd8026ad
JordanSpencerWu <jordan.spencer.wu@gmail.com>
2015-10-07 18:54:09 -0600

    add rspec for trip model

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Trip, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'attributes' do
+    it { should respond_to(:name) }
+    it { should respond_to(:description) }
+    it { should respond_to(:ice_id) }
+    it { should have_many(:checklists) }
+    it { should have_many(:contacts) }
+    it { should have_many(:reservations) }
+    it { should belong_to(:user) }
+    it { should serialize(:ice_id) }
+    it { should validate_presence_of(:name) }
+  end
 end
