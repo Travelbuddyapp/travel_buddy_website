3f0b392432137e7a988c58dbd58293abc963d345
JordanSpencerWu <jordan.spencer.wu@gmail.com>
2015-10-07 19:12:29 -0600

    add rspec for address model

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Address, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'attributes' do
+    it { should respond_to(:latitude) }
+    it { should respond_to(:longitude) }
+    it { should respond_to(:address) }
+    it { should validate_presence_of(:address) }
+    it { should belong_to(:user) }
+    it { should belong_to(:reservation) }
+  end
 end
