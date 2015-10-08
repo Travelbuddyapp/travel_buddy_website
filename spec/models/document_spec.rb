bd50c6f574f9e2950703b282aa1d2fe23845daeb
JordanSpencerWu <jordan.spencer.wu@gmail.com>
2015-10-07 19:03:12 -0600

    add rspec for document model

--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Document, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'attributes' do
+    it { should respond_to(:name) }
+    it { should respond_to(:expiration_date) }
+    it { should belong_to(:user) }
+    it { should validate_presence_of(:name) }
+  end
 end
