6dd6a6298d0684543114a7826cecd248ce8219da
bondparkerbond <parkerbond@gmail.com>
2015-10-13 17:09:19 -0600

    fix rails helper.rb and start user controller specs

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,31 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+let(:user){FactoryGirl.create(:user)}
+
+  describe "GET #show" do
+    it "shows a user" do
+      # expect(User.count).to eq(0)
+      # user1 = user
+      # users = create_list(:user, 1, first_name: 'Doctor Spock')
+      # doctors = create_list(:doctor, 1, name: 'Doctor McCoy', userhm_id: user2.id)
+      # get :show
+      binding.pry
+      # expect(User.count).to eq(1)
+      # expect(user1.first_name).to eq('Doctor Spock')
+    end
+  end
 
   describe "GET #index" do
     it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #edit" do
-    it "returns http success" do
-      get :edit
+      get :index, {id: user.id}
       expect(response).to have_http_status(:success)
     end
   end
