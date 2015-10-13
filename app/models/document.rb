3f8838a871cd561777266d1174a19018a178b058
JordanSpencerWu <jordan.spencer.wu@gmail.com>
2015-10-13 17:23:39 -0600

    workin pdf in form

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,7 +1,6 @@
 class Document < ActiveRecord::Base
   belongs_to :user
   validates :name, presence: true
-  has_attached_file :attachment, styles: { medium: "230x230=", thumb: "100x100>" }
+  has_attached_file :attachment
   validates_attachment :attachment, :content_type => {:content_type => %w(image/jpeg image/jpg image/png application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document)}
-  
 end
