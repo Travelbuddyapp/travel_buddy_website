d08f974ca0db674ececa3169d98a737c5f31e738
JordanSpencerWu <jordan.spencer.wu@gmail.com>
2015-10-13 16:41:01 -0600

    add document form

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,35 +1,57 @@
 class DocumentsController < ApplicationController
   before_action :find_document, only: [:show, :edit, :update, :destroy]
+  before_action :user
 
   def index
     @documents = Document.all
   end
 
   def show
-    @documents = Document.where
   end
 
   def new
+    @document = Document.new
   end
 
   def create
+    @document = Document.new(document_params)
+    @document.user = @user
+    binding.pry
+    if @document.save
+      redirect_to user_documents_path(@user)
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @document.update(document_params)
+      redirect_to user_documents_path(@user)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @document.destroy
+    redirect_to user_documents_path(@user)
   end
 
   private
 
   def document_params
+    params.require(:document).permit(:name, :expiration_date, :attachment)
   end
 
   def find_document
+    @document = Document.find(params[:id])
+  end
+
+  def user
+    @user = current_user
   end
 
 end
--- app/models/document.rb
+++ app/models/document.rb
@@ -1,7 +1,7 @@
 class Document < ActiveRecord::Base
   belongs_to :user
   validates :name, presence: true
-  has_attached_file :attachment, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :attachment, content_type: /\Aimage\/.*\Z/
+  has_attached_file :attachment, styles: { medium: "230x230=", thumb: "100x100>" }
+  validates_attachment :attachment, :content_type => {:content_type => %w(image/jpeg image/jpg image/png application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document)}
   
 end
