8963b617c741f0e6e21c9246a32ec5848fe8e8c9
Will <will@thefixitgeek.com>
2015-10-13 15:24:49 -0600

    toggled off require permission

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :authenticate_user!
-  before_filter :require_permission
+  # before_filter :require_permission
   before_action :configure_permitted_parameters, if: :devise_controller?
  
   protected
