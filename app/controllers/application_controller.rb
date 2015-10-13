08fca8520402c2c7fff17a67a263b76ae3d916c9
Will <will@thefixitgeek.com>
2015-10-12 18:27:18 -0600

    Allow about page without sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  before_filter :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
  
   protected
