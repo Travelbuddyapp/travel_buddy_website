58e3a620c2e8f7de831e6214ecbde5b17822bced
Will <will@thefixitgeek.com>
2015-10-14 18:06:37 -0600

    update home page

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -2,8 +2,16 @@ class StaticPageController < ApplicationController
   skip_before_filter :authenticate_user!, only: [:about_us]
   def home
     @user = current_user
+    @trip = Trip.all
+    @reservation = Reservation.all
+    @checklist = Checklist.all
   end
 
   def about_us
   end
+
+  def trip_params
+    params.require(:user).permit(:name, :description, :start_date, :end_date, :ice_id)
+  end
+
 end
