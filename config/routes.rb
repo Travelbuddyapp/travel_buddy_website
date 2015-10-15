c3f01bd058f117fd55aaebdfb0e5feafb2f7b032
bondparkerbond <parkerbond@gmail.com>
2015-10-15 16:02:19 -0600

    remove user routes and add account static page

--- config/routes.rb
+++ config/routes.rb
@@ -1,41 +1,42 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'static_page#home'
+  root 'static_pages#home'
   get "documents/pdf"
-  get '/about', to: 'static_page#about_us', as: :about 
-  resources :users do
-    resources :addresses # done here and under reservations
-    resources :contacts
-    resources :documents
-    resources :vaccines
-    resources :trips do
-      resources :reservations
-      resources :lodging, controller: 'reservations', type: 'Lodging' do
-        resources :addresses # done twice STI inheritance.
-      end
-      resources :travel, controller: 'reservations', type: 'Travel' do
-        resources :addresses # done twice STI inheritance.
-      end
-      resources :vehicle, controller: 'reservations', type: 'Vehicle' do
-        resources :addresses # done twice STI inheritance.
-      end
-      resources :dining, controller: 'reservations', type: 'Dining' do 
-        resources :addresses # done twice STI inheritance.
-      end
-      resources :activities, controller: 'reservations', type: 'Activity' do 
-        resources :addresses # done twice STI inheritance.
-      end
-      resources :events, controller: 'reservations', type: 'Event' do 
-        resources :addresses # done twice STI inheritance.
-      end
-      resources :other, controller: 'reservations', type: 'Other' do 
-        resources :addresses # done twice STI inheritance.
-      end #end of addresses nested inside of reservation
-      resources :checklists do
-        resources :list_items
-      end # end of cheklist/list item do loop
-    end # end of trips nesting loop
-  end # end of users nesting.
+  get '/about', to: 'static_pages#about_us', as: :about
+  get '/account', to: 'static_pages#account', as: :account
+
+  resources :addresses # done here and under reservations
+  resources :contacts
+  resources :documents
+  resources :vaccines
+  resources :trips do
+    resources :reservations
+    resources :lodging, controller: 'reservations', type: 'Lodging' do
+      resources :addresses # done twice STI inheritance.
+    end
+    resources :travel, controller: 'reservations', type: 'Travel' do
+      resources :addresses # done twice STI inheritance.
+    end
+    resources :vehicle, controller: 'reservations', type: 'Vehicle' do
+      resources :addresses # done twice STI inheritance.
+    end
+    resources :dining, controller: 'reservations', type: 'Dining' do 
+      resources :addresses # done twice STI inheritance.
+    end
+    resources :activities, controller: 'reservations', type: 'Activity' do 
+      resources :addresses # done twice STI inheritance.
+    end
+    resources :events, controller: 'reservations', type: 'Event' do 
+      resources :addresses # done twice STI inheritance.
+    end
+    resources :other, controller: 'reservations', type: 'Other' do 
+      resources :addresses # done twice STI inheritance.
+    end #end of addresses nested inside of reservation
+    resources :checklists do
+      resources :list_items
+    end # end of cheklist/list item do loop
+  end # end of trips nesting loop
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- spec/views/static_pages/about_us.html.erb_spec.rb
+++ spec/views/static_pages/about_us.html.erb_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe "static_page/about_us.html.erb", type: :view do
+RSpec.describe "static_pages/about_us.html.erb", type: :view do
   pending "add some examples to (or delete) #{__FILE__}"
 end
