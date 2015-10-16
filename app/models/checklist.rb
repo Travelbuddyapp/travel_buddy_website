a1501204bfc13bd9f460d21bbb6cdec95206386e
JordanSpencerWu <jordan.spencer.wu@gmail.com>
2015-10-16 15:54:33 -0600

    remove cocoon gem

--- app/models/checklist.rb
+++ app/models/checklist.rb
@@ -3,6 +3,4 @@ class Checklist < ActiveRecord::Base
   belongs_to :user
   has_many :list_items, dependent: :destroy
   validates :title, presence: true
-    accepts_nested_attributes_for :list_items, allow_destroy: true , reject_if: proc { |attributes| attributes['list_items'].blank? }
-  
 end
