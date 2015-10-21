53c09a4a321a2a8fe13fcd40e584670b0411b4d0
JordanSpencerWu <jordan.spencer.wu@gmail.com>
2015-10-21 17:27:07 -0600

    fixing forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,8 @@ class User < ActiveRecord::Base
 
   validates :first_name, :last_name, :birth_date, :phone_number, :gender, presence: true
   has_attached_file :avatar, styles: { medium: "230x230=", thumb: "100x100>" },
-   default_url: "/images/:style/default_avatar.jpg",
-   :url  => ":s3_domain_url",
+    default_url: "/images/:style/default_avatar.jpg",
+    :url  => ":s3_domain_url",
     :path => "public/avatars/:id/:style_:basename.:extension",
     :storage => :fog,
     :fog_credentials => {
@@ -22,8 +22,6 @@ class User < ActiveRecord::Base
         aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
     },
     fog_directory: ENV["FOG_DIRECTORY"]
-
-
 
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
   
