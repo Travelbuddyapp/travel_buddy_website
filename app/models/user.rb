be79e980c0bfa5e0a51ae3a7dc6f69a85346d2e8
Will <will@thefixitgeek.com>
2015-10-21 11:19:53 -0600

    start aws setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,19 @@ class User < ActiveRecord::Base
   has_one :address, dependent: :destroy
 
   validates :first_name, :last_name, :birth_date, :phone_number, :gender, presence: true
-  has_attached_file :avatar, styles: { medium: "230x230=", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :avatar, styles: { medium: "230x230=", thumb: "100x100>" },
+   default_url: "/images/:style/missing.png",
+   :url  => ":s3_domain_url",
+    :path => "public/avatars/:id/:style_:basename.:extension",
+    :storage => :fog,
+    :fog_credentials => {
+        provider: 'AWS',
+        aws_access_key_id: "key_id",
+        aws_secret_access_key: "access_key"
+    },
+    fog_directory: "rails-demo-env"
+
+
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
   
 end
