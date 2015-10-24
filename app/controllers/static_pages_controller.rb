809d52a712ea2f2421566eeee64d1f5a2c5d68da
JordanSpencerWu <jordan.spencer.wu@gmail.com>
2015-10-23 21:57:33 -0600

    separated reservation partial

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,7 +4,7 @@ class StaticPagesController < ApplicationController
 
   def home
     @trips = Trip.where(user_id:@user)
-    @reservaions = Reservation.where(user_id:current_user).order(:check_in_date).limit(5)
+    @reservations = Reservation.where(user_id:current_user).order(:check_in_date).limit(5)
     @checklists = Checklist.where(user_id:current_user).order(:due_date).limit(5)
   end
 
