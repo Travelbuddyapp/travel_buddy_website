51e3f2a5875fd3d5c55c510228d9467f72def437
Will <will@thefixitgeek.com>
2015-10-27 19:47:30 -0600

    swipe left side for mobile navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,7 +27,7 @@ $(document).ready(function() {
    // navbar
   $(".button-collapse").sideNav({
     menuWidth: 300, // Default is 240
-    edge: 'right', // Choose the horizontal origin
+    edge: 'left', // Choose the horizontal origin
     closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
   });
 
