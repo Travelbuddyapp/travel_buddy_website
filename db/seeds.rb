cd8a78eec80d4441b279fca40c835a016d5ac6a0
Will <will@thefixitgeek.com>
2015-11-17 23:25:59 -0700

    fix active_admin css bug

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-AdminUser.create!(email: 'willard@travelbuddy.us', password: 'password', password_confirmation: 'password')
+AdminUser.create!(email: 'hireus@travelbuddyapp.us', password: 'password', password_confirmation: 'password')
